ab6c56f1f3dd512a614beb0898f6e98b55d17fe0
SSHSRN <62781914+SSHSRN@users.noreply.github.com>
2022-02-24 07:54:21 +0530

    updated validation and added a method to clear all

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var isOperator = 0;
 var op = document.getElementById('op');
 let operator = ['+','-','*','/','.'];
 let operands = [];
+let expression = [];
 let operand = "";
 let operators = [];
-console.log(operators);
+var clicked = false;
 
-
-function display(a){
-    op.value = a;
-}
-
+// Function to validate a value before entering it into the expression.
 function validate(i) {
     let l = op.value.length;
     // console.log(l);
     // console.log(op.value[l-1]);
-    if ((operator.includes(op.value[l-1])) && (operator.includes(i))){
+    if(op.value === '' && operator.includes(i)){
+        return false;
+    }
+    else if ((operator.includes(op.value[l-1])) && (operator.includes(i))){
          return false;
         }
     else{
@@ -28,9 +28,12 @@ function validate(i) {
 function append(i){
     let v = validate(i);
     if(v){
+        clicked = false;
         if(operator.includes(i)){
             operators.push(i);
             operands.push(operand);
+            expression.push(operand);
+            expression.push(i);
             operand = "";
         }
         else{
@@ -38,4 +41,24 @@ function append(i){
         }
         op.value += i;
     }
+}
+
+function enter() {
+    if(!clicked){
+        if (operator.includes(op.value[op.value.length-1])){
+            alert("The last value of an expression should not be an operator.");
+        }
+        else{
+            clicked = true;
+            operands.push(operand);
+            expression.push(operand);
+        }
+    }
+}
+
+function clearAll() {
+    op.value = '';
+    operands = [];
+    operators = [];
+    operand = "";
 }
